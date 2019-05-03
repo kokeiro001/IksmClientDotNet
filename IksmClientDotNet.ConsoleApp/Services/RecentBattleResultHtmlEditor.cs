6e30d02a71b900c7897360ae1453cc71e59ef8cb
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-05-03 19:38:55 +0900

    move RecentBattleResultHtmlEditor

--- IksmClientDotNet.ConsoleApp/Services/RecentBattleResultHtmlEditor.cs
+++ IksmClientDotNet.ConsoleApp/Services/RecentBattleResultHtmlEditor.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace IksmClientDotNet.Core.Services
+namespace IksmClientDotNet.ConsoleApp.Services
 {
     public class RecentBattleResultHtmlEditor
     {
