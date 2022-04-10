b94f48a03fb0f8f1a7ab0ee1725b77eaa9d08e38
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2022-04-10 12:45:31 +0900

    add test project

---
+++ IksmClientDotNet2.Tests/Services/IksmClientTests.cs
@@ -0,0 +1,26 @@
+﻿using IksmClientDotNet.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IksmClientDotNet.Core.Services.Tests
+{
+    [TestClass()]
+    public class IksmClientTests
+    {
+        [TestMethod()]
+        public async Task GetBattleResultsTest()
+        {
+            var iksmClient = new IksmClient("hoge");
+
+            var battleResults = await iksmClient.GetBattleResults();
+
+            Assert.IsNotNull(battleResults);
+            Assert.IsNotNull(battleResults.Results);
+            Assert.IsTrue(battleResults.Results.Length <= 50);
+        }
+    }
+}
